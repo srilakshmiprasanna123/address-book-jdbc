1301c1781db73a3e3bcdf0959277b3dad9950b40
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-04-25 01:38:16 +0530

    retrive thwe contact

--- src/main/java/com/db/AddressBookService.java
+++ src/main/java/com/db/AddressBookService.java
@@ -1,7 +1,8 @@
 package com.db;
 
+import java.time.LocalDate;
 import java.util.*;
-import com.db.Person;
+
 
 public class AddressBookService {
 
@@ -28,5 +29,33 @@ public class AddressBookService {
         if (ioService.equals(IOService.DB_IO))
             this.personList = addressBookDBService.readData();
         return personList;
+    }
+
+    public void updateContactNumber(String firstName, String mobileNumber) {
+        int result = addressBookDBService.updateContactNumber(firstName, mobileNumber);
+        if (result == 0) return;
+        Person person = this.getAddressBookData(firstName);
+        if (person != null)
+            person.mobileNumber = mobileNumber;
+
+
+    }
+
+    public Person getAddressBookData(String firstName) {
+        return this.personList.stream()
+                .filter(addressBookDataItem -> addressBookDataItem.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean checkAddressBookInSyncWithDB(String firstName) {
+        List<Person> personList = addressBookDBService.getaddressBookData(firstName);
+        return personList.get(0).equals(getAddressBookData(firstName));
+    }
+
+    public List<Person> readAddressBookForDateRange(IOService ioService, LocalDate startDate, LocalDate endDate) {
+        if (ioService.equals(IOService.DB_IO))
+            return addressBookDBService.getAddressBookForDateRange(startDate, endDate);
+        return null;
     }
 }
